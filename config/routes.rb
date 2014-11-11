b08c657e4b8cf0397b2fec694d6323361589482b
Fadhil <tamin_ghyntach@hotmail.com>
2014-11-11 09:17:02 +0000

    updated routs

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
-  get 'welcome' => 'static_page#welcome'
-  get 'about' => 'static_page#about'
+  get 'welcome' => 'static_pages#welcome'
+  get 'about' => 'static_pages#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
